88e7a81d39095ec8af2c6b0456f8e4140a663b92
Igor Kuznetsov <igorkuznetsov14@gmail.com>
2020-11-22 12:56:12 +0300

    Main page modified

--- tours/views.py
+++ tours/views.py
@@ -1,10 +1,20 @@
+import random
 from django.shortcuts import render
 
 from data import tours
 
 
 def main_view(request):
-    return render(request, "tours/index.html")
+    context_data = {}
+    inds = list(range(1, 17))
+    random.shuffle(inds)
+    for i, tour_i in enumerate(inds[:6]):
+        context_data[f"preview_{i+1}"] = {
+                "title": tours[tour_i]["title"],
+                "link": tours[tour_i]["picture"],
+                "description": tours[tour_i]["description"][:80] + "...",
+            }
+    return render(request, "tours/index.html", context=context_data)
 
 
 def departure_view(request, departure):
@@ -13,7 +23,6 @@ def departure_view(request, departure):
 
 def tour_view(request, id):
     context = tours.get(id)
-    print("context: ", context)
     context["stars_str"] = "★" * int(context["stars"])
     if not context:
         return render(request, '404.html', status=400)
